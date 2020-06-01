6a449cddf25fefaaa04243de753cbcf69f86b2e7
Hao-Zheng-1996 <61253680+Hao-Zheng-1996@users.noreply.github.com>
2020-06-01 16:13:53 -0700

    simple_gamble

---
+++ simple_gamble.py
@@ -0,0 +1,10 @@
+from numpy.random import randint
+
+
+your_value = randint(0,10,1)
+enemy_value  = randint (0,10,1)
+
+if your_value >= enemy_value:
+    print("you win")
+else:
+    print("you loss")
